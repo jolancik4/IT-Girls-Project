60384777e5ef5b8bff761a0d6628ca16f465c1c4
jolancik4 <jolancik4@tlen.pl>
2014-12-02 20:17:57 +0100

    Create project sceleton

---
+++ Herbs/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Herbs::Application.config.secret_key_base = '638bfc60fba965768e3939155b2a61ea6eb28de12bc66209e2a598869b967f90a0d812722c13337f2c1b9108a28799b7816bd49bb15279ebffa851a52becacc8'
