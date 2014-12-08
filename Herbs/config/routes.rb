4ee157ae24306f4191cd6b14a40115e34b48a348
Jana Pejic <jana.pejic.9@gmail.com>
2014-12-08 09:52:04 +0100

    Revert "First steps"
    This reverts commit 7751e03e44448ffce5b4d3584d7819f6db55dec2.

--- Herbs/config/routes.rb
+++ Herbs/config/routes.rb
@@ -1,6 +1,4 @@
 Herbs::Application.routes.draw do
-  resources :products
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
