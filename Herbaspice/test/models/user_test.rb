b4f55730f48744a9682b4dd2841d213fba927399
jolancik4 <jolancik4@tlen.pl>
2015-01-29 19:52:14 +0100

    Working on users model

--- Herbaspice/test/models/user_test.rb
+++ Herbaspice/test/models/user_test.rb
@@ -5,3 +5,17 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
 end
+
+	test "should be valid" do
+    	assert @user.valid?
+  	end
+	test "name should be present" do
+   		@user.name = "     "
+    	assert_not @user.valid?
+  	end
+
+ 	test "name should not be too long" do
+    	@user.name = "a" * 51
+    	assert_not @user.valid?
+  	end
+
